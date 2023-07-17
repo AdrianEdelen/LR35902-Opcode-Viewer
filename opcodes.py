58a7dc7c67286087f9555d8e0b7570522d193993
adrian <adrianedelen@gmail.com>
2023-07-17 11:23:43 -0400

    added prefixed opcodes and restructured the window

--- opcodes.py
+++ opcodes.py
@@ -14,6 +14,7 @@ import json
 with open('C:\\Users\\Adrian\\Documents\\GameBoy\\emulator\\src\\opcodes.json') as f:
     data = json.load(f)
     instructions = []
+    instructionPrefixed = []
     for code, details in data['unprefixed'].items():
         newInstr = instruction()
         newInstr.opcode = code
@@ -27,18 +28,45 @@ with open('C:\\Users\\Adrian\\Documents\\GameBoy\\emulator\\src\\opcodes.json') 
         newInstr.operandOne = details.get('operand1')
         newInstr.operandTwo = details.get('operand2')
         instructions.append(newInstr)
+    for code, details in data['cbprefixed'].items():
+        newInstr = instruction()
+        newInstr.opcode = code
+
+        newInstr.mnemonic = details['mnemonic']
+        newInstr.length = details['length']
+        newInstr.cycles = details['cycles']
+        newInstr.flags = details['flags']
+        newInstr.addr = details['addr']
+        newInstr.group = details['group']
+        newInstr.operandOne = details.get('operand1')
+        newInstr.operandTwo = details.get('operand2')
+        instructionPrefixed.append(newInstr)
 
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import font as tkfont
 
+root = tk.Tk()
+root.geometry("500x500")
+default_font = tkfont.nametofont("TkDefaultFont")
+default_font.configure(size=20)
+
+
+
+
+
 # Keep track of the currently displayed instruction
-current_index = -1
+current_index = 0
+prefixed = tk.IntVar()
+
 
 def display_instruction(index):
-    global current_index
+    global current_index, prefixed
     current_index = index
-    instruction = instructions[index]
+    if prefixed.get() == 1:
+        instruction = instructionPrefixed[index]
+    else:
+        instruction = instructions[index]
     text = f"""
     Opcode: {instruction.opcode}
     Mnemonic: {instruction.mnemonic}
@@ -75,30 +103,41 @@ def previous_instruction():
     else:
         messagebox.showinfo("Error", "No previous instruction")
 
+def update():
+    global current_index
+    display_instruction(current_index)
+
 # Sort instructions by opcode
 instructions.sort(key=lambda instr: int(instr.opcode, 16))
 
-root = tk.Tk()
-root.geometry("800x400")
-default_font = tkfont.nametofont("TkDefaultFont")
-default_font.configure(size=20)
+search_frame = tk.Frame(root)
+search_frame.pack(side=tk.TOP, fill=tk.X)
 
-opcode_label = tk.Label(root, text="0x")
+button_frame = tk.Frame(root)
+button_frame.pack(side=tk.TOP, fill=tk.X)
+
+checkbox_frame = tk.Frame(root)
+checkbox_frame.pack(side=tk.TOP, fill=tk.X)
+
+opcode_label = tk.Label(search_frame, text="0x")
 opcode_label.pack(side=tk.LEFT)
 
-opcode_entry = tk.Entry(root)
+opcode_entry = tk.Entry(search_frame)
 opcode_entry.pack(side=tk.LEFT)
 
-previous_button = tk.Button(root, text="Previous", command=previous_instruction)
+previous_button = tk.Button(button_frame, text="Previous", command=previous_instruction)
 previous_button.pack(side=tk.LEFT)
 
-search_button = tk.Button(root, text="Search", command=search_opcode)
+search_button = tk.Button(button_frame, text="Search", command=search_opcode)
 search_button.pack(side=tk.LEFT)
 
-next_button = tk.Button(root, text="Next", command=next_instruction)
+next_button = tk.Button(button_frame, text="Next", command=next_instruction)
 next_button.pack(side=tk.LEFT)
+
+c1 = tk.Checkbutton(checkbox_frame, text='prefixed',variable=prefixed, onvalue=1, offvalue=0, command=update)
+c1.pack()
 
 result_text = tk.Text(root, font=('Helvetica', '20'))
 result_text.pack()
-
+root.after_idle(update)
 root.mainloop()
